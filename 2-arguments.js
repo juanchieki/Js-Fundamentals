fb44834b207f8ee33bd110ceb0f583734111456c
Juanita <juanchieki@gmail.com>
2025-05-24 15:36:18 -0700

    Add 2-arguments.js script

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+// Check how many command-line arguments are passed
+if (process.argv.length <= 2) {
+  console.log("No argument");
+} else if (process.argv.length === 3) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
